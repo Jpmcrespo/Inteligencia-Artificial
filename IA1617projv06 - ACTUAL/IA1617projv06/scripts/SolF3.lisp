52d5464e0f1b1950bac4d5fb4b743a507e4a56d9
MiraoDaSilva <mirarodrigo@hotmail.com>
2016-12-04 22:37:14 +0000

    falta 3.2 b

--- IA1617projv06 - ACTUAL/IA1617projv06/scripts/SolF3.lisp
+++ IA1617projv06 - ACTUAL/IA1617projv06/scripts/SolF3.lisp
@@ -32,7 +32,6 @@
 (defun getTrackContent (pos track)
   (nth (pos-c pos) (nth (pos-l pos) (track-env track))))
 
-;; Pedir 0,4
 (defun isObstaclep (pos track)
   "check if the position pos is an obstacle"
   (or (< (pos-l pos) 0) (< (pos-c pos) 0)
@@ -40,34 +39,33 @@
       (>= (pos-c pos) (pos-c (track-size track)))
       (null (getTrackContent pos track))))
 
-;; Pedir 0,4
 (defun isGoalp (st) 
   "check if st is a solution of the problem"
   (let ((current-position (state-pos st))
-	(track (state-track st)))
+  (track (state-track st)))
     (and (member current-position (track-endpositions track) :test #'equalp)
-	 T)))
+   T)))
 
-;; Pedir 1,2
+
+
 (defun nextState (st act)
   "generate the nextState after state st and action act from prolem"
   (let ((new-state (make-state :action act :track (state-track st))))
     (setf (state-vel new-state)
-	  (make-vel (+ (vel-l (state-vel st)) (acce-l act))
-		    (+ (vel-c (state-vel st)) (acce-c act))))
+    (make-vel (+ (vel-l (state-vel st)) (acce-l act))
+        (+ (vel-c (state-vel st)) (acce-c act))))
     (setf (state-pos new-state)
-	  (make-pos (+ (pos-l (state-pos st)) (vel-l (state-vel new-state)))
-		    (+ (pos-c (state-pos st)) (vel-c (state-vel new-state)))))
+    (make-pos (+ (pos-l (state-pos st)) (vel-l (state-vel new-state)))
+        (+ (pos-c (state-pos st)) (vel-c (state-vel new-state)))))
     (setf (state-cost new-state)
-	  (cond ((isGoalp new-state) -100)
-		((isObstaclep (state-pos new-state) (state-track new-state)) 20)
-		(T 1)))
+    (cond ((isGoalp new-state) -100)
+    ((isObstaclep (state-pos new-state) (state-track new-state)) 20)
+    (T 1)))
     (when (= (state-cost new-state) 20)
       (setf (state-vel new-state) (make-vel 0 0))
       (setf (state-pos new-state) (make-pos (pos-l (state-pos st))
-					    (pos-c (state-pos st)))))
+              (pos-c (state-pos st)))))
     (values new-state)))
-
 ;; Solution of phase 2
 
 ;;; Pedir 
@@ -208,7 +206,9 @@
     do (let*  ( (expansionNode (findLowestF openList))
                 (nextSt (funcall(problem-fn-nextStates problem) (node-state expansionNode)))
               )
-      (remove expansionNode openList)
+      (print openList)
+      (delete expansionNode openList)
+      (push expansionNode closedList)
       (when (funcall(problem-fn-isGoal problem) (node-state expansionNode))
         (return-from a* (solution expansionNode))
         )
@@ -223,15 +223,14 @@
                       :f (+ g h)
                       ))
                 (flag nil)
-                ) 
-            (dolist (newNode (append openList closedList))
-              (when (equal (state-pos (node-state newNode)) (state-pos (node-state successor)))
-                (setf flag t))
+                )
+            (dolist (oldNode (append openList closedList))
+              (when (equal (state-pos (node-state oldNode)) (state-pos (node-state successor)))
+                (setf flag t) )
               )
             (when (not flag) (push successor openList))  
           )
         )
-      (push expansionNode closedList)
 
       )
 
@@ -253,6 +252,10 @@
         (setf finalNode el)
       )
     )
+  (print minimumF)
+  (print (state-pos (node-state finalNode)))
+  (print (state-vel (node-state finalNode)))
+
   (return-from findLowestF finalNode)
 )
 )
---
+++ IA1617projv06 - ACTUAL/IA1617projv06/scripts/projF4.lisp
@@ -0,0 +1,32 @@
+;;; this is your solution file
+(load "SolF3.lisp")
+
+(defun states-to-list (stts)
+  (loop for st in stts
+	  collect (format nil "POS: ~a VEL: ~a ACT: ~a COST: ~a~&"
+	  (state-pos st)  (state-vel st)  (state-action st)  (state-cost st))))
+
+(defun initial-state (track)
+  (make-state :pos (track-startpos track) :vel (make-vel 0 0) :action nil :cost 0 :track track))
+
+(defvar *t1* nil)
+(defvar *p1* nil)
+
+(setf *t1* (loadtrack "track0.txt"))
+
+(setf *p1* (make-problem :initial-state (initial-state *t1*)  :fn-isGoal #'isGoalp	  :fn-nextstates #'nextStates	  :fn-h #'compute-heuristic))
+			  
+	
+(defvar *t2* nil)
+(defvar *p2* nil)
+
+(setf *t2* (loadtrack "track9.txt"))
+
+(setf *p2* (make-problem :initial-state (initial-state *t2*)  :fn-isGoal #'isGoalp	  :fn-nextstates #'nextStates	  :fn-h #'compute-heuristic))
+			  
+(format t "~&Exercise 3.2b - A*~&")
+ (let ((real1 (get-internal-real-time)))
+		 (with-open-file (str "out3.2b.txt" :direction :input)
+	   (format t "~% Solution is correct? ~a~&" (string= (format nil "~{~a~^~}" (states-to-list (a* *p2*))) (read str))))
+      (let ((real2 (get-internal-real-time)))
+	  (format t "~%Computation took: ~f seconds of real time~%" (/ (- real2 real1) internal-time-units-per-second))))
